e5cf2ddc03a149e2d105f87160f695accdcb3eeb
Harmani25 <farouqtunde@gmail.com>
2024-03-13 21:28:10 +0000

    apply

--- app/app.js
+++ app/app.js
@@ -124,6 +124,6 @@ app.get("/hello/:name", function(req, res) {
 });
 
 // Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
+app.listen(2000,function(){
+    console.log(`Server running at http://127.0.0.1:2000/`);
 });
